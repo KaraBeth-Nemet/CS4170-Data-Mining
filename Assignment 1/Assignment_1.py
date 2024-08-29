e28451144e7f01c960df4f65199739325cfe7a70
KaraBeth <43915118+KaraBeth-Nemet@users.noreply.github.com>
2024-08-29 15:40:44 +0000

    Learning stuff

--- Assignment 1/Assignment_1.py
+++ Assignment 1/Assignment_1.py
@@ -1,5 +1,9 @@
 import csv
 with open('mutations.csv', mode ='r')as file:
   csvFile = csv.reader(file)
+  individual_sample_number = 0
+  
   for lines in csvFile:
-        print(lines)
+    individual_sample_number += 1
+
+print("The number of individual samples in the file is: ", individual_sample_number)
