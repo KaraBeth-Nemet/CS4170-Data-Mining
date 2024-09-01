2842ec3368a612095bcd48c231c10370a1fa64c4
KaraBeth <43915118+KaraBeth-Nemet@users.noreply.github.com>
2024-09-01 19:12:40 +0000

    finished data anaylsis and added comments

--- Assignment 1/Assignment_1.py
+++
@@ -1,9 +0,0 @@
-import csv
-with open('mutations.csv', mode ='r')as file:
-  csvFile = csv.reader(file)
-  individual_sample_number = 0
-  
-  for lines in csvFile:
-    individual_sample_number += 1
-
-print("The number of individual samples in the file is: ", individual_sample_number)
